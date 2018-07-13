c6e6741fe98c06ffbb720070e3722c5e7536ab28
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-07-13 19:48:24 +0200

    Añado el controlador User y la clase UserType

---
+++ ASP.NET-Core Formularios de datos/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET_Core_Formularios_de_datos.Models;
+using ASP.NET_Core_Formularios_de_datos.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Formularios_de_datos.Controllers
+{
+    [Route("user")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("edit")]
+        public IActionResult Edit()
+        {
+            ViewBag.UserTypes = UserType.GetUserTypes(); 
+            return View(new UserViewModel());
+        }
+    }
+}
